3da53ddb39bb704c6d153fb59fb06e3a8ee17958
okiyuki99 <motoyuki.oki@dr-ubie.com>
2025-01-06 21:15:04 +0900

    オープンデータを利用している旨を追加

--- choufu_geo_folium.py
+++ choufu_geo_folium.py
@@ -39,6 +39,12 @@ st.set_page_config(
   layout="wide"
 )
 
+st.markdown("""
+# 調布市の人口ヒートマップ
+
+オープンデータをもとにした調布市の市区町村別の人口をヒートマップで可視化したアプリケーションです
+""")
+
 # mapの用意（佐須町二丁目を中心に）
 lat = 35.660076
 lon = 139.554033
@@ -90,3 +96,11 @@ map.keep_in_front(choropleth_info)
 
 # 可視化実行
 st_folium(map, use_container_width=True, height=800, returned_objects=[])
+
+st.markdown("""
+### 利用データについて
+このアプリケーションで使われているデータは以下のオープンデータ（CC-BY-4.0ライセンス）を利用して作成しています
+
+* [調布市の世帯と人口に関するデータ](https://www.city.chofu.lg.jp/030040/p017111.html)より調布市の町別の人口データをダウンロード
+* [国勢調査町丁・字等別境界データセット](https://geoshape.ex.nii.ac.jp/ka/resource/)より調布市のTopoJSONファイルをダウンロード
+""")
